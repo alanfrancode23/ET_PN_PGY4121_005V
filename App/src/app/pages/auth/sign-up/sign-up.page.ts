f4db2e7dcc42e98d4d64b6dfa8315a52eb42dc70
Joan Toro Ortiz <joantoro@me.com>
2023-08-21 21:04:59 -0400

    Formulario Sign-Up OK!

---
+++ App/src/app/pages/auth/sign-up/sign-up.page.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { CustomValidators } from 'src/app/utils/custom-validators';
+
+@Component({
+  selector: 'app-sign-up',
+  templateUrl: './sign-up.page.html',
+  styleUrls: ['./sign-up.page.scss'],
+})
+export class SignUpPage implements OnInit {
+
+  form = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.minLength(4)]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required]),
+    confirmPassword: new FormControl(''),
+  });
+
+  constructor() { }
+
+  ngOnInit() {
+    this.confirmPasswordValidator();
+  }
+
+  confirmPasswordValidator() {
+    this.form.controls.confirmPassword.setValidators([
+      Validators.required,
+      CustomValidators.matchValues(this.form.controls.password)
+    ])
+    this.form.controls.confirmPassword.updateValueAndValidity();
+  }
+
+  submit() {
+    if (this.form.valid) {
+      console.log(this.form.value);
+    }
+  }
+
+}
